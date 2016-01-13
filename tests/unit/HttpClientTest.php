00dc45c21d44a305c3bda6b05a7fe05c000a83bd
Bubba <rob@stechstudio.com>
2016-01-13 16:31:29 -0500

    First HTTP Client Unit Test

--- tests/unit/HttpClientTest.php
+++ tests/unit/HttpClientTest.php
@@ -1,24 +1,32 @@
 <?php
-
+use RC\Sdk\HttpClient;
 
 class HttpClientTest extends \Codeception\TestCase\Test
 {
+    use \Codeception\Specify;
+
     /**
-     * @var \UnitTester
+     * @var HttpClient
      */
-    protected $tester;
-
-    protected function _before()
-    {
-    }
-
-    protected function _after()
-    {
-    }
+    private $httpClient;
 
     // tests
-    public function testMe()
+    public function testInstantiation()
     {
+        $this->httpClient = new HttpClient();
 
+        $this->specify("No Request Middleware", function(){
+         $this->assertEmpty($this->httpClient->getRequestMiddleware());
+        });
+
+        $this->specify("No Response Middleware", function(){
+            $this->assertEmpty($this->httpClient->getResponseMiddleware());
+        });
+
+        $this->specify("Auto Generates a Guzzle Client", function(){
+            $this->assertEquals(GuzzleHttp\Client::class, get_class($this->httpClient->getGuzzle()));
+        });
     }
+
+    
 }

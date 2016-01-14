b46e7359135aefedf5dd03996097022d7172deb8
Bubba <rob@stechstudio.com>
2016-01-14 13:25:01 -0500

    Cool! Mock Guzzle. Test Send Request

---
+++ tests/unit/sdk/Pipeline/SendRequestTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Sdk\Pipeline;
+
+use Illuminate\Container\Container;
+use RC\Sdk\HttpClient;
+use RC\Sdk\Pipeline\SendRequest;
+use RC\Sdk\Request;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+
+class SendRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiation()
+    {
+        $testBody = ["some" =>"test"];
+        $mock = new MockHandler([
+            new Response(200, ['X-Foo' => 'Bar'], json_encode($testBody)),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+
+        $baseUrl = 'http://php.unit/test';
+        $config = [
+            "httpMethod" => "POST",
+            "uri" => "/oazwsdob",
+            "parameters" => [
+                "domain" => [
+                    "validate" => "required|string",
+                    "location" => "body"
+                ],
+                "id" => [
+                    "validate" => "required|numeric",
+                    "location" => "uri"
+                ]
+            ]
+        ];
+        $arguments = ['foz', 'baz', 'sheesh'];
+
+        $requestDTO = new Request($client, $baseUrl, $config, $arguments);
+        $requestDTO->url = 'http://php.unit/test/oazwsdob';
+        $sendRequests = new SendRequest();
+        $request = $sendRequests->handle($requestDTO, function($request){return $request;});
+        $this->assertEquals($testBody, $request->responseBody);
+    }
+}

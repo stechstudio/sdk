d0d7aef9b622f9dcfe48a621d337b8ff87a21f62
Joseph Szobody <joseph@stechstudio.com>
2016-01-15 14:17:17 -0500

    exception handling

---
+++ src/Exceptions/ApiResponseException.php
@@ -0,0 +1,48 @@
+<?php
+namespace RC\Sdk\Exceptions;
+
+use Exception;
+
+/**
+ * Class ApiResponseException
+ * @package RC\Sdk\Exceptions
+ */
+class ApiResponseException extends Exception
+{
+    /**
+     * @var int HTTP Status code
+     */
+    protected $status = 400;
+
+    /**
+     * Just to be a bit more explicit about what code we're talking about
+     * @return int|mixed
+     */
+    public function getErrorCode()
+    {
+        return $this->getCode();
+    }
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    /**
+     * Just to be a bit more explicit about what code we're talking about
+     * @return mixed
+     */
+    public function getHttpStatusCode()
+    {
+        return $this->getStatus();
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorName()
+    {
+        return (new \ReflectionClass($this))->getShortName();
+    }
+}

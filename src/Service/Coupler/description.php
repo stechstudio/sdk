0ce992af9a28efa19e6d6f0b5d4fe563c9c77b8f
Joseph Szobody <joseph@stechstudio.com>
2016-01-15 17:36:24 -0500

    coupler

---
+++ src/Service/Coupler/description.php
@@ -0,0 +1,62 @@
+<?php
+return [
+    'baseUrl' => isset($_ENV['COUPLER_URL']) ? $_ENV['COUPLER_URL'] : 'http://coupler.vpc.rc',
+    'operations' => [
+        'syncEfsFile' => [
+            'httpMethod' => 'POST',
+            'uri' => isset($_ENV['COUPLER_MQ_WEBHOOK']) ? $_ENV['COUPLER_MQ_WEBHOOK'] : 'https://mq-aws-us-east-1-1.iron.io/3/projects/569967544826aa000600005f/queues/push-qa/webhook?oauth=18AlckkDW2kdMRx1Lx50hw1XR4g',
+            'parameters' => [
+                "domain" => [
+                    "validate" => "required|string",
+                    "location" => "body"
+                ],
+                "path" => [
+                    "validate" => "required|string",
+                    "location" => "body"
+                ]
+            ],
+        ],
+        'getAuthorizeUrl' => [
+            'httpMethod' => 'GET',
+            'uri' => '/authorization-url',
+            'parameters' => [
+                'domain' => [
+                    'validate' => 'required',
+                    'location' => 'query'
+                ],
+                'redirectUri' => [
+                    'validate' => 'required',
+                    'location' => 'query',
+                ]
+            ],
+        ],
+        'finishAuthorization' => [
+            'httpMethod' => 'POST',
+            'uri' => '/integrations',
+            'parameters' => [
+                'redirectUri' => [
+                    'validate' => 'required',
+                    'location' => 'body',
+                ],
+                'csrfToken' => [
+                    'validate' => 'required',
+                    'location' => 'body',
+                ],
+                'queryParams' => [
+                    'validate' => 'required',
+                    'location' => 'body',
+                ],
+            ],
+        ],
+        'retrieveAuthorization' => [
+            'httpMethod' => 'GET',
+            'uri' => '/integrations/{id}',
+            'parameters' => [
+                'id' => [
+                    'validate' => 'required',
+                    'location' => 'uri',
+                ]
+            ],
+        ],
+    ]
+];

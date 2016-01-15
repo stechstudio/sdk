f6be267a199f378205d613f56eb436241f505cf4
Bubba <rob@stechstudio.com>
2016-01-15 09:49:39 -0500

    Initial validation tests

---
+++ tests/unit/sdk/Pipeline/ValidateArgumentsTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Sdk\Pipeline;
+
+use Illuminate\Container\Container;
+use RC\Sdk\HttpClient;
+use RC\Sdk\Pipeline\BuildBody;
+use RC\Sdk\Pipeline\ValidateArguments;
+use RC\Sdk\Request;
+
+class ValidateArgumentsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiation()
+    {
+        $client = new HttpClient(new Container());
+        $baseUrl = 'http://php.unit/test';
+        $config = [
+            "httpMethod" => "POST",
+            "uri" => "/oazwsdob",
+            "parameters" => [
+                "domain" => [
+                    "validate" => "required|string",
+                    "location" => "body"
+                ],
+                "id" => [
+                    "validate" => "required|numeric",
+                    "location" => "uri"
+                ]
+            ]
+        ];
+        $arguments = ["id"=>1, "domain"=>"php.unit"];
+
+        $requestDTO = new Request($client, $baseUrl, $config, $arguments);
+        $validateArg = new ValidateArguments();
+        $request = $validateArg->handle($requestDTO, function($request){return $request;});
+        $this->assertEquals(Request::class, get_class($request), 'We should get a valided request object back.');
+    }
+}

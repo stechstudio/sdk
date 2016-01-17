21b32d8ef09d9665db687c0bfbf0591879282fbf
Joseph Szobody <joseph@stechstudio.com>
2016-01-16 21:40:40 -0500

    update the pipeline

--- src/Pipeline/BuildBody.php
+++ src/Pipeline/BuildBody.php
@@ -18,10 +18,21 @@ class BuildBody
      */
     public function handle(Request $request, Closure $next)
     {
-        $bodyArguments = $request->getArguments("body");
+        $jsonData = $request->getOperation()->getDataByLocation("json");
+        $bodyData = $request->getOperation()->getDataByLocation("body");
 
-        $request->setBody(json_encode($bodyArguments));
-        $request->setHeader("Content-Type","application/json");
+        if(count($jsonData)) {
+            // We strongly prefer json. If you give us any json at all, it's all we'll use.
+            $request->setBody(json_encode($jsonData));
+            $request->setHeader("Content-Type","application/json");
+
+        } else if(count($bodyData)) {
+            // Otherwise we'll use body data, but I'm not really sure how to do this. Just stick the raw
+            // values in as post data? Should I add a line break inbetween each one?
+            $request->setBody(implode('', $bodyData));
+        }
+
+        // TODO: add support for postField data (application/x-www-form-urlencoded).
 
         return $next($request);
     }
